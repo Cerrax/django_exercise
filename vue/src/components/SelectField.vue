e7a25b4f9d8ee17dc09e7817451c1a2a7af5b1c1
Charlie Koch <charles@cerrax.com>
2024-11-26 09:56:54 -0500

    Add new select field

---
+++ vue/src/components/SelectField.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  items: {
+	default: []
+  },
+})
+
+const value = defineModel()
+const isDisabled = ref(props.disabled)
+
+defineExpose({
+  value,
+  isDisabled,
+})
+</script>
+
+<template>
+  <div class="input-field">
+    <label v-if="label" :for="id" >{{ label }}: </label>
+    <select :id="id" :name="name" v-model="value">
+		<option disabled value="">---</option>
+		<option v-for="item in items" :key="item.pk" :value="item.pk">
+			{{ item.name }}
+		</option>
+	</select>
+  </div>
+</template>
+
+<style scoped>
+</style>
